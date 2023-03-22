0dd47b6531658bf48e08a5cf41e4273d02358931
Renan Dutra <r.granatodutra@gmail.com>
2023-03-21 21:24:58 -0300

    changed return string

--- hackaton.py
+++ hackaton.py
@@ -4,11 +4,11 @@ hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
 
 def get_pos(number):
     if(number == 1):
-        return '1st'
+        return 'winner!'
     if(number == 2):
-        return '2nd'
+        return '2nd place.'
     if(number == 3):
-        return '3rd'
+        return '3rd place.'
     return f'{number}th'
 
 def get_score (team, tournament):
@@ -20,10 +20,10 @@ def get_score (team, tournament):
 
 
 print(get_score("Team Ateliware", hackathon_1))
-#Team Ateliware was the 2nd
+#Team Ateliware was winner!
 
 print(get_score("Team Mirum", hackathon_2))
-#Team Mirum was the 4th
+#Team Mirum was the 4th place.
 
 print(get_score("Team Kenzie", hackathon_3))
-#Team Kenzie was the 4th
+#Team Kenzie was the 4th place.
